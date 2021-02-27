ddb9d65631623c9af7e453918a2964fc9a4d10b1
Srinivas Peeta <srinivaspt@outlook.com>
2021-02-27 11:55:51 +0530

    Init

---
+++ src/test/java/com/edge/edgebnb/EdgebnbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edge.edgebnb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdgebnbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
